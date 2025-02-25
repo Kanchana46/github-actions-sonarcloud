56e3f66c8f6f939793a6ce4af512b72b9770cc18
Kanchana <kanchana.rdg@gmail.com>
2025-02-25 19:29:57 +0530

    first commit

---
+++ src/main/java/com/code/ghsonar/GhsonarApplication.java
@@ -0,0 +1,13 @@
+package com.code.ghsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhsonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhsonarApplication.class, args);
+	}
+
+}
